0b7057bc24a496870c0d18412d94952b9f9294ff
Domonique Taylor <taylord651@gmail.com>
2020-04-05 19:16:48 -0500

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // TODO: add code here
+window.addEventListener('load', function() {
+    
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            const container = document.getElementById('container');
+            let index = 0;
+        
+            container.innerHTML = `
+                <div class="astronaut">
+                <div class="bio">
+                    <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                    <ul>
+                    <li>Hours in space: ${json[index].hoursInSpace}</li>
+                    <li>Active: ${json[index].active}</li>
+                    <li>Skills: ${json[index].skills.join(", ")}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src=${json[index].picture}/>
+                </div>
+            `;
+    });
+  
+  });
+}) 
